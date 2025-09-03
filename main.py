d8a50cca9ace93c516c6b8433164ad363f4ef030
tsitohainassandatia <hei.sandatia@gmail.com>
2025-09-03 09:57:31 +0300

    question 1-b

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import Response
 from starlette.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,6 +12,12 @@ def ping():
     return Response ("OK")
 
 # b-
-@app.post('/phones')
-def phones(id: str = "non defini"):
-    return JSONResponse( {id},status_code=201)
+class phone(BaseModel):
+    identifiant: str
+    brand: str
+    model: str
+
+
+@app.post("/phones")
+def post_phone(request: phone):
+    return JSONResponse({request.identifiant},{request.brand}, {request.model}, status_code=201)
